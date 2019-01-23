36fc845d2a374ffeda841b769bfc52c304369f49
Kevin Lefevre <kevin.lefevre@snips.ai>
2019-01-23 11:26:28 +0100

    Fix compat with rust 1.31

--- ffi-utils/src/lib.rs
+++ ffi-utils/src/lib.rs
@@ -2,7 +2,7 @@ mod conversions;
 mod errors;
 mod types;
 
-pub use conversions::*;
-pub use errors::*;
+pub use self::conversions::*;
+pub use self::errors::*;
+pub use self::types::*;
 pub use failure_utils::display::ErrorExt;
-pub use types::*;
